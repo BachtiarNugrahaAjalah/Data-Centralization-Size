e27da773f3e847b02a4bf6f4a89b375d681c7e11
Tiarlah <bahtiarvivo123@gmail.com>
2024-10-20 14:43:31 +0700

    main.cpp + mean

--- main.cpp
+++ main.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include "mean.h"
 
+using namespace std;
+
+int main(){
+    char inputStart;
+    
+    do
+    {
+    printf("Mulai? [Y/n] : ");
+    cin >> inputStart;
+
+        int jumlahData;
+        printf("Masukkan Jumlah Data : ");
+        cin >> jumlahData;
+
+        double long* data = new double long [jumlahData];
+        for(int i = 0; i < jumlahData; i++){
+            cout << "Masukkan Data Ke-" << i+1 << " : ";
+            cin >> data[i];
+        }
+
+        double long mean = Mean(data, jumlahData);
+        
+    } while (inputStart != 'y' || inputStart != 'Y');
+}
